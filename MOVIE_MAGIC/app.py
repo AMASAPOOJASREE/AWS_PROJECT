42f28cb904f0c33ea41be7cda30ff1c4ed74dea4
AMASAPOOJASREE <apsreddy10@gmail.com>
2025-07-04 14:46:05 +0530

    Update app.py

--- MOVIE_MAGIC/app.py
+++ MOVIE_MAGIC/app.py
@@ -210,5 +210,5 @@ def reset_password():
         return "User not found."
     return render_template('reset_password.html')
 
-if _name_ == '_main_':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
